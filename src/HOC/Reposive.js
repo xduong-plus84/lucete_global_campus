e161b297e76c6082fe931eb7a79c98c88a1b7954
Duong Nguyen <nguyenxuanduongxd2k@gmail.com>
2023-01-17 10:34:41 +0700

    remove tablet view and mobile view

--- src/HOC/Reposive.js
+++ src/HOC/Reposive.js
@@ -1,11 +1,11 @@
 import { useMediaQuery } from "react-responsive";
 
 export const DesktopView = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 1500 });
+  const isDesktop = useMediaQuery({ minWidth: 1000 });
   return isDesktop ? children : null;
 };
 export const TabletView = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1499 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 999 });
   return isTablet ? children : null;
 };
 export const MobileView = ({ children }) => {
